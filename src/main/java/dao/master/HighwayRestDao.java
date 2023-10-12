ca371f53d16bcc97b0a40a51769c709d5186a9ba
Jungkook Yoo <yoojeebee@kakao.com>
2023-10-12 12:16:36 +0900

    --- 고속도로 및 휴게소 기능 update

---
+++ src/main/java/dao/master/HighwayRestDao.java
@@ -0,0 +1,21 @@
+package dao.master;
+
+import data.entity.HighwayRest;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.ArrayList;
+
+@Mapper
+public interface HighwayRestDao {
+    @Insert("insert into highwayrest values(seq_highwayrest_no.nextval, #{svarCd}, #{svarNm}, #{gudClssCd}, #{rprsTelNo}, #{routeCd})")
+    void insert(HighwayRest item);
+
+    @Select("select svarCd from highwayrest where svarCd = #{svarCd}")
+    String select(@Param("svarCd") String svarCd);
+
+    @Select("select * from highwayrest")
+    ArrayList<HighwayRest> selectAll();
+}
