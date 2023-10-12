696521283c537fda0accdf114c620da2445e097d
Jungkook Yoo <yoojeebee@kakao.com>
2023-10-12 18:27:00 +0900

    --- 공통 파일 수정 및 고속도로 관련 파일 수정

---
+++ src/main/java/common/FileSearch.java
@@ -0,0 +1,95 @@
+package common;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class FileSearch {
+    public static void fileSearchAndConstructor(String packageName, Map<String, Handler> pathList) {
+        String packageNameSlash = "./" + packageName.replace(".", "/");
+
+        URL directoryURL = Thread.currentThread().getContextClassLoader().getResource(packageNameSlash);
+
+        String directoryString = directoryURL.getFile();
+
+        File file = new File(directoryString);
+        if(file.exists()) {
+            list(file.listFiles(), packageName, pathList);
+        }
+    }
+
+    public static void list(File[] fileList, String packageName, Map<String, Handler> pathList) {
+        for(File f : fileList) {
+            if(f.getName().endsWith(".class")) {
+                String fileName = f.getName();
+                fileName = fileName.substring(0, fileName.length() - 6);
+                try{
+                    Class c = Class.forName(packageName + "." + fileName);
+                    Constructor<?> constructor = c.getConstructor(null);
+                    try {
+                        Handler obj = (Handler)constructor.newInstance();
+                        pathList.put(obj.getPath(), obj);
+                        System.out.println("obj: " + obj);
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                } catch (ClassNotFoundException | NoSuchMethodException e){
+                    System.err.println(packageName + "." + fileName + " does not appear to be a valid class");
+                    System.out.println(e.getMessage());
+                }
+            }
+            else {
+                String oPackageName = packageName;
+                oPackageName += "." +f.getName();
+                list(f.listFiles(), oPackageName, pathList);
+            }
+        }
+    }
+
+    public static void fileSearchAndMapperReturn(String packageName, ArrayList<Class<?>> list) {
+        System.out.println("통과!");
+        String packageNameSlash = "./" + packageName.replace(".", "/");
+        System.out.println("packageNameSlash: " + packageNameSlash);
+
+        URL directoryURL = Thread.currentThread().getContextClassLoader().getResource(packageNameSlash);
+        System.out.println("url: " + directoryURL);
+
+        String directoryString = directoryURL.getFile();
+        System.out.println("directoryString: " + directoryString);
+
+        File file = new File(directoryString);
+        if(file.exists()) {
+            mapperReturn(file.listFiles(), packageName, list);
+        }
+        System.out.println("list.size(): " + list.size());
+        System.out.println("끝");
+    }
+
+    public static void mapperReturn(File[] fileList, String packageName, ArrayList<Class<?>> list) {
+        for(File f : fileList) {
+//            System.out.println("fileNm: " + f.getName());
+            if(f.getName().endsWith(".class")) {
+                String fileName = f.getName();
+                fileName = fileName.substring(0, fileName.length() - 6);
+                System.out.println("className: " + packageName + "." + f.getName());
+                try{
+                    Class<?> cl = Class.forName(packageName + "." + fileName);
+                    list.add(cl);
+                } catch (ClassNotFoundException e){
+                    System.err.println(packageName + "." + fileName + " does not appear to be a valid class");
+                    System.out.println(e.getMessage());
+                }
+            }
+            else {
+                String oPackageName = packageName;
+                oPackageName += "." +f.getName();
+                mapperReturn(f.listFiles(), oPackageName, list);
+            }
+        }
+    }
+}
--- src/main/java/dao/master/RestFoodDao.java
+++ src/main/java/dao/master/RestFoodDao.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface FoodDao {
-    @Insert("insert into restfood(no, name, stdRestCd) values(seq_restfood_no.nextval, #{name}, #{stdRestCd})")
+public interface RestFoodDao {
+    @Insert("insert into restfood(no, name, stdRestCd, foodCost) values(seq_restfood_no.nextval, #{name}, #{stdRestCd}, #{foodCost})")
     void insert(RestFood restFood);
 }
